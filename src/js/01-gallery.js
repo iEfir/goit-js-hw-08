2f98e036f826ed1b5565ed3d4542e32a536b9bdd
Kravchenko Yevhenii <111376447+iEfir@users.noreply.github.com>
2023-01-28 09:26:05 +0200

    task-01 done

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,40 @@
-// Add imports above this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 import { galleryItems } from './gallery-items';
-// Change code below this line
 
-console.log(galleryItems);
+const galleryEl = document.querySelector('.gallery');
+
+const onImgClick = event => {
+  event.preventDefault();
+  if (!event.target.classList.contains('gallery__image')) {
+    return;
+  }
+
+  function closeModal(event) {
+    if (event.code === 'Escape') {
+      lightbox.close();
+    }
+  }
+};
+galleryEl.addEventListener('click', onImgClick);
+
+const createGalletyElements = galleryItems.map(element => {
+  const link = document.createElement('a');
+  link.classList.add('gallery__item');
+  link.href = `${element.original}`;
+
+  const image = document.createElement('img');
+  image.classList.add('gallery__image');
+  image.src = `${element.preview}`;
+  image.alt = `${element.description}`;
+
+  link.appendChild(image);
+  return link;
+});
+
+galleryEl.append(...createGalletyElements);
+
+const simpleLightbox = new SimpleLightbox('.gallery a', {
+  captionDelay: 250,
+  captionsData: 'alt',
+});
