e274e6d4e92081039c39fc6dd86e3ea6c326fb19
Kravchenko Yevhenii <111376447+iEfir@users.noreply.github.com>
2023-01-29 18:17:00 +0200

    task-03 done

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,34 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+const formData = {};
+const STOREAGE_KEY = 'feedback-form-state';
+
+form.addEventListener('input', throttle(onInput, 500));
+form.addEventListener('submit', onSubmit);
+
+condition();
+
+function onInput(e) {
+  formData[e.target.name] = e.target.value;
+  localStorage.setItem(STOREAGE_KEY, JSON.stringify(formData));
+}
+
+function onSubmit(e) {
+  e.preventDefault();
+  const dataStoreage = JSON.parse(localStorage.getItem(STOREAGE_KEY));
+  console.log('dataStoreage', dataStoreage);
+  e.currentTarget.reset();
+  localStorage.removeItem(STOREAGE_KEY);
+}
+
+function condition() {
+  const dataStoreage = JSON.parse(localStorage.getItem(STOREAGE_KEY));
+  const email = document.querySelector('input');
+  const message = document.querySelector('textarea');
+
+  if (dataStoreage) {
+    email.value = dataStoreage.email;
+    message.value = dataStoreage.message;
+  }
+}
