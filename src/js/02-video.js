7863d57ac87f0ad9b4a6ca7741e31efaa1e56eeb
Kravchenko Yevhenii <111376447+iEfir@users.noreply.github.com>
2023-01-30 16:50:22 +0200

    fix

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -2,18 +2,17 @@ import throttle from 'lodash.throttle';
 
 const iframe = document.querySelector('iframe');
 const player = new Vimeo.Player(iframe);
+const STOREAGE_KEY = 'videoplayer-current-time';
 
 player.on('timeupdate', throttle(onPlayer, 1000));
 
 function onPlayer({ seconds }) {
-  localStorage.setItem('videoplayer-current-time', seconds);
+  localStorage.setItem(STOREAGE_KEY, seconds);
 }
 
-try {
-  player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
-} catch (error) {
-  console.log(error.name);
-  console.log(error.message);
+const dataStoreage = localStorage.getItem(STOREAGE_KEY);
+if (dataStoreage) {
+  player.setCurrentTime(dataStoreage);
 }
 
 // player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
