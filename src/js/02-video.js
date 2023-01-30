c983cb714eaf05de8856b949c08c3cc91e95c451
Kravchenko Yevhenii <111376447+iEfir@users.noreply.github.com>
2023-01-30 16:56:03 +0200

    fix

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -4,15 +4,18 @@ const iframe = document.querySelector('iframe');
 const player = new Vimeo.Player(iframe);
 const STOREAGE_KEY = 'videoplayer-current-time';
 
+load();
 player.on('timeupdate', throttle(onPlayer, 1000));
 
 function onPlayer({ seconds }) {
   localStorage.setItem(STOREAGE_KEY, seconds);
 }
 
-const dataStoreage = localStorage.getItem(STOREAGE_KEY);
-if (dataStoreage) {
-  player.setCurrentTime(dataStoreage);
+function load() {
+  const dataStoreage = localStorage.getItem(STOREAGE_KEY);
+  if (dataStoreage) {
+    player.setCurrentTime(dataStoreage);
+  }
 }
 
 // player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
